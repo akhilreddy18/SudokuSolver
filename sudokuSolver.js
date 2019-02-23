6984e05d9830efd03c5c2f23eced495bde6b0438
Akhil Reddy <akhilreddy18@outlook.com>
2019-02-23 14:52:21 +0100

    added puzzles file and can now read from it

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -1,58 +1,74 @@
-function sudokuSolver(puzzle){
-	var filledPosition={}, impossibleNumbers, emptySpaces = 81;
-	while(emptySpaces > 0){
-		emptySpaces= 0;
-	for(var col=0; col<puzzle.length; col++){
-		for(var row=0; row<puzzle.length; row++){
-			// var colB = Math.floor(col/3)*3;
-			// var rowBox = Math.floor(row/3)*3;
-			if(puzzle[col][row] === 0){
-				emptySpaces
-				filledPosition={};
-				for(var i=0; i<9; i++){
-					if(puzzle[col][i] > 0){
-						filledPosition[puzzle[col][i]] = true;
-					}
-					if(puzzle[i][row] > 0){
-						filledPosition[puzzle[i][row]] = true;
-					}
-				}
-					for(var colBox = Math.floor(col/3)*3;colBox < Math.floor(col/3)*3+3; colBox++){
-						for(var rowBox = Math.floor(row/3)*3;rowBox < Math.floor(row/3)*3+3; rowBox++){
-							if(puzzle[colBox][rowBox] > 0){
-								filledPosition[puzzle[colBox][rowBox]] = true;
+var fs = require("fs");
+
+var allPuzzles = fs.readFileSync('puzzles.txt', 'utf8').split("Grid");
+allPuzzles.splice(0,1);
+
+var randomPuzzle = new Array(9);
+for(var i=0; i<9; i++){
+	randomPuzzle[i] = new Array(9);
+}
+
+for(var i=0; i<9; i++){
+	for(var j=0; j<9; j++){
+		randomPuzzle[i][j] = parseInt(allPuzzles[0].split("\n")[i+1].charAt(j));
+	}
+} 
+
+console.log(randomPuzzle);
+console.log("--------------------------------");
+console.log(sudokuSolver(randomPuzzle));
+
+function sudokuSolver(randomPuzzle){
+	
+	var filledNumbers=[], emptySpaces=true;
+
+	while(emptySpaces){
+
+		emptySpaces=false;
+		for(var col=0; col<9; col++){
+			for(var row=0; row<9; row++){
+				
+				if(randomPuzzle[col][row] === 0){
+
+					emptySpaces=true;
+					filledNumbers=[];
+					for(var i = 0; i<9; i++){
+
+						if(randomPuzzle[col][i]>0){
+							if(!filledNumbers.includes(randomPuzzle[col][i])){
+								filledNumbers.push(randomPuzzle[col][i]);
+							}
+						}
+
+						if(randomPuzzle[i][row]>0){
+							if(!filledNumbers.includes(randomPuzzle[i][row])){
+								filledNumbers.push(randomPuzzle[i][row]);
 							}
 						}
 					}
-				impossibleNumbers = Object.keys(filledPosition);
-				if(impossibleNumbers.length === 8) {
-					for(var i=1; i<10; i++){
-						if(impossibleNumbers.indexOf(i.toString())<0){
-							puzzle[col][row] = i;
+
+					for(var colBox=Math.floor(col/3)*3; colBox< Math.floor(col/3)*3+3; colBox++){
+						for(var rowBox=Math.floor(row/3)*3; rowBox<Math.floor(row/3)*3+3; rowBox++){
+							if(randomPuzzle[colBox][rowBox] > 0){
+								if(!filledNumbers.includes(randomPuzzle[colBox][rowBox])){
+									filledNumbers.push(randomPuzzle[colBox][rowBox]);
+								}
+							}
 						}
 					}
-				} else {
-					emptySpaces++;
-				}
 
+					if(filledNumbers.length === 8){
+						for(var i=1; i<10; i++){
+							if(!filledNumbers.includes(i)){
+								randomPuzzle[col][row] = i;
+								// console.log(filledNumbers);
+								// console.log([col,row], i,i);
+							}
+						}
+					}
+				}
 			}
 		}
 	}
+	return randomPuzzle;
 }
-	return puzzle;
-}
-
-
-
-var puzzle = [
-[5,3,0,0,7,0,0,0,0],
-[6,0,0,1,9,5,0,0,0],
-[0,9,8,0,0,0,0,6,0],
-[8,0,0,0,6,0,0,0,3],
-[4,0,0,8,0,3,0,0,1],
-[7,0,0,0,2,0,0,0,6],
-[0,6,0,0,0,0,2,8,0],
-[0,0,0,4,1,9,0,0,5],
-[0,0,0,0,8,0,0,7,9]];
-
-console.log(sudokuSolver(puzzle));
